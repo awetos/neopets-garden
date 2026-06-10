0ed94f7224b237f85bb503ea15d28d9a567da9ab
awetos <awetosprouts@gmail.com>
2026-06-10 18:10:09 -0500

    Suspense boundary and main stats.

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -1,8 +1,16 @@
+import MainStatsCard from "@/components/all-stats/stats-page-components/main-stats-card";
+import { Suspense } from "react";
+
 export default function Stats() {
   return (
-    <div>
-      Some stats about the gardening results will be here. Such as fragment drop
-      rate and most commonly dropped items
-    </div>
+    <>
+      <div>
+        Some stats about the gardening results will be here. Such as fragment
+        drop rate and most commonly dropped items
+      </div>
+      <Suspense>
+        <MainStatsCard />
+      </Suspense>
+    </>
   );
 }
---
+++ components/all-stats/stats-page-components/main-stats-card.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const { formatDistance } = require("date-fns");
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { AllStatsStructure, getLatestStats } from "@/firebase/get-stats";
+
+export default function MainStatsCard() {
+  const router = useRouter();
+
+  const [latestStats, setLatestStats] = useState<AllStatsStructure | null>(
+    null,
+  );
+  const [isLoading, setIsLoading] = useState(true);
+  const searchParams = useSearchParams();
+  const refreshKey = searchParams.get("refresh");
+
+  useEffect(() => {
+    async function loadLatestStats() {
+      const stats_data = await getLatestStats();
+      if (stats_data) {
+        setLatestStats(stats_data);
+      }
+      setIsLoading(false);
+    }
+
+    loadLatestStats();
+  }, [refreshKey]);
+
+  return (
+    <div>
+      {!isLoading && latestStats && (
+        <div className="flex flex-row flex-wrap justify-evenly text-center text-sm font-semibold">
+          <div>
+            <p>
+              {latestStats.totalSeeds.toLocaleString()} total seeds submitted
+            </p>
+          </div>
+          <div>
+            <p>---</p>
+          </div>
+          <div>
+            <p>
+              {latestStats.totalFragments.toLocaleString()} fragments received
+            </p>
+          </div>
+          <div>
+            <p>---</p>
+          </div>
+          <div>
+            <p>
+              fragment drop rate:{" "}
+              {(
+                (latestStats.totalFragments / latestStats.totalSeeds) *
+                100
+              ).toFixed(2)}
+              %
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
