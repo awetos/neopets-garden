0adce23dfa97149f53ce917418849af593141fef
awetos <awetosprouts@gmail.com>
2026-06-10 19:34:49 -0500

    Does firebase get seed stats build?

--- components/all-stats/stats-page-components/all-seeds-container.tsx
+++ components/all-stats/stats-page-components/all-seeds-container.tsx
@@ -1,11 +1,14 @@
 "use client";
+import { SeedStat } from "@/types/garden-result";
 import { useEffect, useState } from "react";
 
 export default function AllSeedsContainer() {
   const [isLoading, setIsLoading] = useState<true | false>(true);
+  const [allSeeds, setAllSeeds] = useState<SeedStat[]>([]);
   useEffect(() => {
     async function getSeeds() {
       await new Promise((resolve) => setTimeout(resolve, 2000));
+
       setIsLoading(false);
     }
     getSeeds();
--- firebase/get-seed-stats.ts
+++ firebase/get-seed-stats.ts
@@ -1,14 +1,13 @@
-import {
-  collection,
-  doc,
-  getDocs,
-  getDoc,
-  query,
-  orderBy,
-  limit,
-} from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "./firebase-client";
+import { SeedStat } from "@/types/garden-result";
 export const getSeedsStats = async () => {
   const seedsRef = collection(db, "seeds");
   const seedsSnap = await getDocs(seedsRef);
+  return seedsSnap.docs.map(
+    (doc): SeedStat => ({
+      seed: doc.id,
+      stats: doc.data(),
+    }),
+  );
 };
