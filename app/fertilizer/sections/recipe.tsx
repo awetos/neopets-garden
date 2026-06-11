865a509d79aeb142ac8ffa86ea80e1a6ae39c739
awetos <awetosprouts@gmail.com>
2026-06-11 15:17:38 -0500

    removed list and other sections

--- app/fertilizer/sections/recipe.tsx
+++ app/fertilizer/sections/recipe.tsx
@@ -7,35 +7,25 @@ export default function Recipes() {
       <div className="flex flex-row items-center justify-center">
         <img src="/images/fertilizer/cookingpotkeeper.gif"></img>
       </div>
-      <ol className="list-decimal pl-20">
-        <li>
-          Go to the <b>Cooking Pot</b> on Mystery Island.
-        </li>
-        <li>
-          Combine <b>Pile of Dung</b> with
-          <b> Fish</b> to get<b> Basic Compost</b>
-          <br></br>
-          --or--
-          <br></br>
-          Combine <b>Pile of Dung</b> with
-          <b> Bone</b> to get<b> Basic Compost</b>
-        </li>
-        <li>
-          Combine <b>Basic Compost</b> with
-          <b>Berry</b> to get
-          <b> Basic Fertilizer </b>
-          <br></br>
-          --or--
-          <br></br>
-          Combine <b>Basic Compost</b> with
-          <b>Seed</b> to get
-          <b> Basic Fertilizer</b>
-        </li>
-        <li>
-          Combine <b>Basic Fertilizer</b> with <b>Magic Vial</b> to get
-          <b> Miracle Food </b>
-        </li>
-      </ol>
+      Go to the <b>Cooking Pot</b> on Mystery Island. Combine{" "}
+      <b>Pile of Dung</b> with
+      <b> Fish</b> to get<b> Basic Compost</b>
+      <br></br>
+      --or--
+      <br></br>
+      Combine <b>Pile of Dung</b> with
+      <b> Bone</b> to get<b> Basic Compost</b>
+      Combine <b>Basic Compost</b> with
+      <b> Berry</b> to get
+      <b> Basic Fertilizer </b>
+      <br></br>
+      --or--
+      <br></br>
+      Combine <b>Basic Compost</b> with
+      <b>Seed</b> to get
+      <b> Basic Fertilizer</b>
+      Combine <b>Basic Fertilizer</b> with <b>Magic Vial</b> to get
+      <b> Miracle Food </b>
     </div>
   );
 }
