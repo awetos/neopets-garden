5251c8e8a8b324ee864deadf6cd27edaf0af151e
awetos <awetosprouts@gmail.com>
2026-06-11 15:26:13 -0500

    table

--- app/fertilizer/sections/recipe.tsx
+++ app/fertilizer/sections/recipe.tsx
@@ -7,25 +7,48 @@ export default function Recipes() {
       <div className="flex flex-row items-center justify-center">
         <img src="/images/fertilizer/cookingpotkeeper.gif"></img>
       </div>
-      Go to the <b>Cooking Pot</b> on Mystery Island. Combine{" "}
-      <b>Pile of Dung</b> with
-      <b> Fish</b> to get<b> Basic Compost</b>
-      <br></br>
-      --or--
-      <br></br>
-      Combine <b>Pile of Dung</b> with
-      <b> Bone</b> to get<b> Basic Compost</b>
-      Combine <b>Basic Compost</b> with
-      <b> Berry</b> to get
-      <b> Basic Fertilizer </b>
-      <br></br>
-      --or--
-      <br></br>
-      Combine <b>Basic Compost</b> with
-      <b>Seed</b> to get
-      <b> Basic Fertilizer</b>
-      Combine <b>Basic Fertilizer</b> with <b>Magic Vial</b> to get
-      <b> Miracle Food </b>
+      <table className="w-full px-2">
+        <colgroup>
+          <col className="w-1/8" />
+          <col className="w-7/8" />
+        </colgroup>
+        <tbody>
+          <tr className="border-y-2 border-amber-300">
+            <td className="bg-amber-200 text-center">Step 1</td>
+            <td className="text-center">
+              Go to the <b>Cooking Pot</b> on Mystery Island. Combine{" "}
+              <b>Pile of Dung</b> with
+              <b> Fish</b> to get<b> Basic Compost</b>
+              <br></br>
+              --or--
+              <br></br>
+              Combine <b>Pile of Dung</b> with
+              <b> Bone</b> to get<b> Basic Compost</b>
+            </td>
+          </tr>
+          <tr className="border-y-2 border-amber-300">
+            <td className="bg-amber-200 text-center">Step 2</td>
+            <td className="text-center">
+              Combine <b>Basic Compost</b> with
+              <b> Berry</b> to get
+              <b> Basic Fertilizer </b>
+              <br></br>
+              --or--
+              <br></br>
+              Combine <b>Basic Compost</b> with
+              <b>Seed</b> to get
+              <b> Basic Fertilizer</b>
+            </td>
+          </tr>
+          <tr className="border-y-2 border-amber-300">
+            <td className="bg-amber-200 text-center">Step 3</td>
+            <td className="text-center">
+              Combine <b>Basic Fertilizer</b> with <b>Magic Vial</b> to get
+              <b> Miracle Food </b>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
