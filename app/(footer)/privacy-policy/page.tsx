053f8ba7d049bb440b320bddf3bdaa60337e4b5d
awetos <awetosprouts@gmail.com>
2026-06-14 13:03:11 -0500

    Simple a tags intead of Link

--- app/(footer)/privacy-policy/page.tsx
+++ app/(footer)/privacy-policy/page.tsx
@@ -1,18 +1,45 @@
 import Heading from "@/components/about/heading";
-import Link from "next/link";
 
 export default function PrivacyPolicy() {
   return (
     <div>
+      <Heading title="Privacy Policy" />
       <div>
         All submissions are anonymous. Only the gardening results are collected.
-        You can see the exact information collected in submission form
-        (/master/components/submission-form/submission-form.tsx) and the upload
-        to Firebase function (/master/firebase/upload/upload-submission.ts).
+        You can see the exact information collected in{" "}
+        <a
+          className="font-bold text-amber-600 hover:underline"
+          href="https://github.com/awetos/neopets-garden/blob/master/components/submission-form/submission-form.tsx"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          submission form
+        </a>{" "}
+        and the{" "}
+        <a
+          className="font-bold text-amber-600 hover:underline"
+          href="https://github.com/awetos/neopets-garden/blob/master/firebase/upload/upload-submission.ts"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          upload to Firebase function
+        </a>
+        .
       </div>
+
+      <Heading title="Open Source" />
+
       <div>
-        This project is open source. You can view the source code here:
-        https://github.com/awetos/neopets-garden .
+        This project is open source. You can view{" "}
+        <a
+          className="font-bold text-amber-600 hover:underline"
+          href="https://github.com/awetos/neopets-garden"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          the source code here
+        </a>
+        .
       </div>
     </div>
   );
