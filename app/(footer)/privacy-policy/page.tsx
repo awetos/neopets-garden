96ffec40138f845bb46b2b012cc11d704928cc6a
awetos <awetosprouts@gmail.com>
2026-06-14 13:00:48 -0500

    no links at all.

--- app/(footer)/privacy-policy/page.tsx
+++ app/(footer)/privacy-policy/page.tsx
@@ -4,23 +4,15 @@ import Link from "next/link";
 export default function PrivacyPolicy() {
   return (
     <div>
-      <Heading title="Privacy Policy" />
       <div>
         All submissions are anonymous. Only the gardening results are collected.
         You can see the exact information collected in submission form
         (/master/components/submission-form/submission-form.tsx) and the upload
-        to Firebase function (/master/firebase/upload/upload-submission.ts) .
+        to Firebase function (/master/firebase/upload/upload-submission.ts).
       </div>
-      <Heading title="Open Source" />
       <div>
-        This project is open source. You can view{" "}
-        <Link
-          className="font-bold text-amber-600 hover:underline"
-          href="https://github.com/awetos/neopets-garden"
-        >
-          the source code here
-        </Link>
-        .
+        This project is open source. You can view the source code here:
+        https://github.com/awetos/neopets-garden .
       </div>
     </div>
   );
